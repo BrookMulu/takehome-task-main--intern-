8cd8bcdd5cfbf869a7091af82c52a2b1941833ba
BrookMulu <brook.mgeb@gmail.com>
2025-01-01 18:27:27 -0800

    Added my comments on what I would have done to finish my remaining todo's in the api.php file

--- api.php
+++ api.php
@@ -28,3 +28,18 @@ if ( !isset( $_GET['title'] ) && !isset( $_GET['prefixsearch'] ) ) {
 } else {
 	echo json_encode( [ 'content' => $app->fetch( $_GET ) ] );
 }
+
+/**
+ * * If I had more time for the remaining todo's:
+ * TODO A: I would have improved the readability by refracroring the file
+ * into methods and documentating the methods, the methods I would have probably implemented are
+ * hadle requests, handle prefix search and handle title fetch.
+ * TODO B: I would have centralized the route handling using a switch case statement
+ * based on parameters and defining separate methods for each route.
+ * TODO C: I would have cached the results in get list of articles if the results got too bug
+ * I would have used functions like stripos for better string matching. I would have also tried to 
+ * remove loop filtering with array functions for better performance.
+ * TODO D: I would have made sure to input validation and sanitization and prevented directory transversal
+ * like I did in index.php and also set up content security policy for the header like I did in
+ * index.php.
+ */
